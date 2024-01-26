9b1c0bb11def52e02d9cd57077f00ca843589908
Nikhil <nikhil342015@gmail.com>
2024-01-25 17:04:40 -0800

    Project setup

---
+++ main.py
@@ -0,0 +1,12 @@
+# Authors:
+#   Amber Hankins
+#   Nikhil Sharma
+#   CS 431 - Big Data
+#   Topic: Covid-19 Prediction
+
+def main():
+    print('Hello world!')
+
+
+if __name__ == '__main__':
+    main()
