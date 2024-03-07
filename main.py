e5d6a6333978463070374337714081a371d00cb4
NikhilSharma1234 <nikhil342015@gmail.com>
2024-03-07 15:43:40 -0800

    Load CSV with Alabama data

--- main.py
+++ main.py
@@ -5,15 +5,26 @@
 #   Topic: Covid-19 Prediction
 
 import pandas as pd
+import io
+import requests
+import glob
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 def main():
     print('Hello world!')
     fetchData()
 
 def fetchData():
-    url = 'https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports/01-01-2021.csv'
-    df = pd.read_csv(url, index_col=0)
-    print(df.head(5))
+    path = os.getenv('CSVPATH')
+    all_files = glob.glob(path)
+    li = []
+    for filename in all_files:
+        df = pd.read_csv(filename, index_col=None, header=0)
+        li.append(df[df['Province_State'] == 'Alabama'])
+    frame = pd.concat(li, axis=0, ignore_index=True)
+    print(frame.to_string())
 
 if __name__ == '__main__':
     main()
