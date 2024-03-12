71c8fec821e23b91a0dc56919105fd4cdddebabb
Amber Hankins <amberhankins@nevada.unr.edu>
2024-03-11 20:41:52 -0700

    Linear Regression on # of confirmed cases

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # covid-19-prediction
+
+
+
+# REQUIREMENTS
+* pip install -U scikit-learn
+* pip install -U matplotlib
--- main.py
+++ main.py
@@ -5,26 +5,55 @@
 #   Topic: Covid-19 Prediction
 
 import pandas as pd
-import io
-import requests
-import glob
-from dotenv import load_dotenv
-import os
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
-load_dotenv()
+
 def main():
-    print('Hello world!')
-    fetchData()
+    # Convert first five days of 2021 into individual dataframes for training data
+    df_train = pd.DataFrame()
+    df_train = pd.concat([df_train, fetchData('01-01-2021.txt')], ignore_index=True)
+    df_train = pd.concat([df_train, fetchData('01-02-2021.txt')], ignore_index=True)
+    df_train = pd.concat([df_train, fetchData('01-03-2021.txt')], ignore_index=True)
+    df_train = pd.concat([df_train, fetchData('01-04-2021.txt')], ignore_index=True)
+    df_train = pd.concat([df_train, fetchData('01-05-2021.txt')], ignore_index=True)
+    df_train = pd.concat([df_train, fetchData('01-06-2021.txt')], ignore_index=True)
+    df_train = pd.concat([df_train, fetchData('01-07-2021.txt')], ignore_index=True)
+    df_train = pd.concat([df_train, fetchData('01-08-2021.txt')], ignore_index=True)
+    df_train = pd.concat([df_train, fetchData('01-09-2021.txt')], ignore_index=True)
 
-def fetchData():
-    path = os.getenv('CSVPATH')
-    all_files = glob.glob(path)
-    li = []
-    for filename in all_files:
-        df = pd.read_csv(filename, index_col=None, header=0)
-        li.append(df[df['Province_State'] == 'Alabama'])
-    frame = pd.concat(li, axis=0, ignore_index=True)
-    print(frame.to_string())
+
+    # Convert next five days of 2021 into individual dataframes for testing data
+    df_test = fetchData('01-10-2021.txt')
+
+    X_train = df_train.drop(columns=['Confirmed'])
+    X_test = df_test.drop(columns=['Confirmed'])
+
+    y_train = df_train['Confirmed']
+    y_test = df_test['Confirmed']
+
+    reg = LinearRegression()
+    reg.fit(X_train, y_train)
+    preds = reg.predict(X_test)
+    dist = np.linalg.norm(preds-y_test)
+    print("Linear Regression Prediction Precision: " + str(dist))
+    
+
+def fetchData(url):
+    # Read csv into pandas dataframe
+    df = pd.read_csv(url, index_col=0)
+
+    # Drop all columns that contain only NaNs
+    df = df.dropna(axis=1, how='all')
+
+    # Dropping rows that still contain NaNs (Cruise Ships and American Samoa)
+    df = df.dropna(axis=0)
+
+    # Drop redundant and non-numerical rows
+    df = df.drop(columns=['Country_Region', 'Last_Update', 'ISO3', 'Date'])
+
+    # Return dataframe
+    return df
 
 if __name__ == '__main__':
     main()
